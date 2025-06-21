371f5a1c8270497496f5d2e54775cbf541f8f057
Hudrolax <hudro795@gmail.com>
2025-06-20 21:00:12 -0300

    sc

---
+++ langgraph/structured_output.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from pprint import pprint as print
+from typing import Optional
+
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+chain = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+
+class Person(BaseModel):
+    """Данные о человеке"""
+
+    name: Optional[str] = Field(..., description="Имя")
+    surname: Optional[str] = Field(..., description="Фамилия")
+    birth_date: Optional[datetime] = Field(description="Дата рождения")
+    preferences: Optional[str] = Field(description="Предпочтения человека")
+
+
+structured_llm = chain.with_structured_output(Person)
+system_msg = SystemMessage(
+    content="""
+Извлекай данне о человеке из текста и возвращай их в структурированном виде.
+Не придумывай факты. Если данных для поля нет - вставляй None
+"""
+)
+
+
+def preprocessing(data: list[str]) -> list[Person]:
+    result = []
+    for query in data:
+        human_msg = HumanMessage(query)
+        result.append(structured_llm.invoke([system_msg, human_msg]))
+    return result
+
+
+data = [
+    """
+    Что за человек Сиплый Иван? Это тот, которому сегодня ровно 35 лет (а сегодня 20 июня 2025 года).
+    И тот, который любит курить трубку.
+    """,
+    """
+    Что мы знаем о Жоре? Да в принципе ничего
+
+    """,
+]
+
+output = preprocessing(data)
+print(output)
